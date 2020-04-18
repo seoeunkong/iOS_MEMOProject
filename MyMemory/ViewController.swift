fd16a5c67275da2da31d601cd86a7882c24afafd
sqlpro <sqlpro@naver.com>
2020-04-18 23:59:55 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by 꼼꼼한재은님 on 2020/04/18.
+//  Copyright © 2020 꼼꼼한재은님. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
